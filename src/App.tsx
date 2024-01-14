dc186e8f9f892e0ad1ea57bbb616ec2d09ed680e
jyh071116 <jyh071116@gmail.com>
2024-01-14 22:11:46 +0900

    feat: access refresh 토큰 저장

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import Header from "components/Header";
 import React from "react";
 
 function App() {
+  const accessToken = new URLSearchParams(window.location.search).get(
+    "accessToken",
+  );
+  const refreshToken = new URLSearchParams(window.location.search).get(
+    "refreshToken",
+  );
+
+  if (accessToken && refreshToken) {
+    localStorage.setItem("access_token", accessToken);
+    document.cookie = `refresh_token=${refreshToken}`;
+  }
+
   return (
     <div>
       <Header />
