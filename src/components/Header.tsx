cf0102246e3e77f070502aa46467d5eca9d88236
jyh071116 <jyh071116@gmail.com>
2024-01-16 23:22:10 +0900

    feat: 모달 퍼블리싱 & 로직 처리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useUser from "hooks/useUser";
 import logo from "assets/logo.svg";
 import search from "assets/search.svg";
 import userIcon from "assets/userIcon.svg";
+import UserModal from "./UserModal";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { user, isLogin, logout } = useUser();
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      const target = e.target as Node;
+      if (isModalOpen && target instanceof Element && !target.closest('#modal')) {
+        setIsModalOpen(false);
+      }
+    };
+
+    if (isModalOpen) {
+      setTimeout(() => {
+        window.addEventListener("click", handleOutsideClick);
+      }, 100);
+    }
+    return () => {
+      window.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isModalOpen, setIsModalOpen]);
+
   return (
     <div className="flex justify-between items-center px-[50px] h-[56px]">
       <img className="w-[97px]" src={logo} alt="로고" />
@@ -20,19 +43,29 @@ const Header = () => {
       </div>
       <div className="flex justify-end w-[50px] min-w-[225px]">
         <div className="flex flex-1 justify-end">
-          <a
-            className="flex justify-center items-center text-sm text-login-blue h-[36px] px-[15px] border-[1px] border-back/[.1] rounded-full hover:bg-hover-login-bg hover:border-transparent"
-            href="https://youtube.anys34.com/oauth2/authorization/google"
-          >
+          {isLogin ? (
             <img
-              className="ml-[-6px] mr-[6px]"
-              src={userIcon}
-              alt="유저아이콘"
+              className="rounded-full size-[32px] cursor-pointer"
+              src={user.profile}
+              alt="아이콘"
+              onClick={() => setIsModalOpen(true)}
             />
-            로그인
-          </a>
+          ) : (
+            <a
+              className="flex justify-center items-center text-sm text-login-blue h-[36px] px-[15px] border-[1px] border-back/[.1] rounded-full hover:bg-hover-login-bg hover:border-transparent"
+              href="https://youtube.anys34.com/oauth2/authorization/google"
+            >
+              <img
+                className="ml-[-6px] mr-[6px]"
+                src={userIcon}
+                alt="유저아이콘"
+              />
+              로그인
+            </a>
+          )}
         </div>
       </div>
+      {isModalOpen && <UserModal user={user} logout={logout} />}
     </div>
   );
 };
---
+++ src/components/UserModal.tsx
@@ -0,0 +1,47 @@
+import IUser from "interfaces/IUser";
+import logoutImg from "assets/logout.svg";
+import React from "react";
+
+interface UserModalProps {
+  user: IUser;
+  logout: () => void;
+}
+
+const UserModal = ({ user, logout }: UserModalProps) => {
+  return (
+    <div
+      id="modal"
+      className="fixed top-[45px] left-[calc(100%-340px)] w-[300px] bg-white shadow-[0_4px_32px_0_rgba(0,0,0,0.1)] rounded-[12px]"
+    >
+      <div className="flex p-[16px]">
+        <img
+          className="rounded-full size-[40px] cursor-pointer mr-[16px]"
+          src={user.profile}
+          alt="아이콘"
+        />
+        <div>
+          <div>{user.nickname}</div>
+          <div className="mb-[5px] break-all">{user.email}</div>
+          <a
+            className="text-login-blue text-[14px]"
+            href="https://www.youtube.com/channel/UCh5Crce0bSXb-sms3pizhsw"
+          >
+            내 채널 보기
+          </a>
+        </div>
+      </div>
+      <hr />
+      <div className="py-[8px]">
+        <div
+          onClick={logout}
+          className="flex items-center cursor-pointer px-[16px] py-[6px] hover:bg-black/[0.05]"
+        >
+          <img src={logoutImg} alt="로그아웃" />
+          <div className="ml-[16px] text-[14px] text-text-gray">로그아웃</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserModal;
