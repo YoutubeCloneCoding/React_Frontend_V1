ee25b6a170271e2d01e81b7b9f20c5503426edea
jyh071116 <jyh071116@gmail.com>
2024-01-16 23:20:12 +0900

    feat: useUser 커스텀 훅 생성

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import customAxios from "lib/customAxios";
+
+const useUser = () => {
+  const { data: userInfo } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const { data } = await customAxios.get("/api/profile");
+      return data;
+    },
+  });
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    window.location.reload();
+  };
+  return {
+    user: userInfo,
+    isLogin: !!userInfo,
+    logout,
+  };
+};
+
+export default useUser;
