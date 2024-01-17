9eaff5d205f6164ea46dd4c88571f9ee02f19a58
jyh071116 <jyh071116@gmail.com>
2024-01-17 16:42:15 +0900

    feat: post 데이터 삽입

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,11 +7,11 @@ interface PostProps {
 
 const Post = ({ post }: PostProps) => {
   return (
-    <div className="flex flex-1 basis-[350px] flex-col justify-center gap-[12px] mb-[40px]">
+    <div className="flex flex-col justify-start gap-[12px] mb-[40px]">
       <a href="https://www.youtube.com/watch?v=LWA06BY_gsY">
         <img
           className="aspect-video w-full rounded-2xl"
-          src="	https://i.ytimg.com/vi/LWA06BY_gsY/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLDhpC6SYIzwemu0JieV2-QexGopUw"
+          src={post.thumbnail}
           alt="썸네일"
         />
       </a>
@@ -19,12 +19,12 @@ const Post = ({ post }: PostProps) => {
       <a className="flex gap-[12px]" href="https://www.youtube.com/@RALO24">
         <img
           className="size-[36px] rounded-full"
-          src="https://yt3.ggpht.com/ytc/AIf8zZQNWfU6iMvJ_jvC-Th4YlLZP2NTzgUe8Eubv_ZtWA=s68-c-k-c0x00ffffff-no-rj"
+          src={post.profile}
           alt="프로필"
         />
         <div>
-          <div className="text-[16px] font-medium">술안주는 역시 커피땅콩</div>
-          <div className="text-[14px] text-sub-title">랄로</div>
+          <div className="text-[16px] font-medium">{post.title}</div>
+          <div className="text-[14px] text-sub-title">{post.nickname}</div>
           <div className="text-[14px] text-sub-title mt-[-2px]">
             조회수 123만회 · 3주 전
           </div>
