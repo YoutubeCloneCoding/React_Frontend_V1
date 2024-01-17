1fd7d840bcee9b713181d8ecc2af74686f9870cd
jyh071116 <jyh071116@gmail.com>
2024-01-17 15:41:13 +0900

    feat(header): 헤더 모달 만들기 버튼 퍼블리싱

--- src/components/UserModal.tsx
+++ src/components/UserModal.tsx
@@ -1,5 +1,6 @@
 import IUser from "interfaces/IUser";
 import logoutImg from "assets/logout.svg";
+import makeVideo from "assets/makeVideo.svg";
 import React from "react";
 
 interface UserModalProps {
@@ -32,6 +33,14 @@ const UserModal = ({ user, logout }: UserModalProps) => {
       </div>
       <hr />
       <div className="py-[8px]">
+        <div
+          className="flex items-center cursor-pointer px-[16px] py-[6px] hover:bg-black/[0.05]"
+        >
+          <img src={makeVideo} alt="영상 만들기" />
+          <div className="ml-[16px] text-[14px] text-text-gray">만들기</div>
+        </div>
+      </div>
+      <div className="pb-[8px]">
         <div
           onClick={logout}
           className="flex items-center cursor-pointer px-[16px] py-[6px] hover:bg-black/[0.05]"
