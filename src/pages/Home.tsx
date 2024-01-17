092f711e50c01bb9a72e242e04377ce0a9de24e5
jyh071116 <jyh071116@gmail.com>
2024-01-17 15:51:38 +0900

    feat(api): /list api 연결

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Post from "components/Post";
+import { useQuery } from "@tanstack/react-query";
+import customAxios from "lib/customAxios";
+import IPost from "interfaces/IPost";
 
 const Home = () => {
+  const { data: postList } = useQuery({
+    queryKey: ["postList"],
+    queryFn: async () => {
+      const { data } = await customAxios("/list");
+      return data;
+    },
+  });
+  console.log(postList);
+
   return (
     <div className="flex gap-[20px] flex-wrap mx-[50px] mt-[50px]">
-      {Array.from(Array(100)).map(() => (
-        <Post />
+      {postList?.map((post: IPost) => (
+        <Post post={post} />
       ))}
     </div>
   );
