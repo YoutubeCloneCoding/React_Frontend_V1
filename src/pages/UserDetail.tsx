c33a7dc6ae83554bbde572e8f09b2826b4c62ecb
jyh071116 <jyh071116@gmail.com>
2024-01-19 18:32:00 +0900

    feat: 유저 세부페이지 api연결 완료

--- src/pages/UserDetail.tsx
+++ src/pages/UserDetail.tsx
@@ -1,4 +1,3 @@
-import useUser from "hooks/useUser";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import customAxios from "lib/customAxios";
@@ -6,17 +5,26 @@ import IPost from "interfaces/IPost";
 import getAgo from "helpers/getAgo";
 import Post from "components/Post";
 import dayjs from "dayjs";
+import IUser from "interfaces/IUser";
 
 const UserDetail = () => {
   const { email } = useParams();
-  const { user } = useUser();
-  const { data: postList } = useQuery({
-    queryKey: ["postList"],
+
+  const { data: userPostList } = useQuery<IPost[]>({
+    queryKey: [`post${email}`],
     queryFn: async () => {
-      const { data } = await customAxios("/list");
+      const { data } = await customAxios(`/userList?email=${email}`);
       return data.map((post: IPost) => {
         return { ...post, createdAt: getAgo(dayjs(post.createdAt)) };
       });
+    },
+  });
+
+  const { data: user } = useQuery<IUser>({
+    queryKey: [`user${email}`],
+    queryFn: async () => {
+      const { data } = await customAxios(`/profile/${email}`);
+      return data;
     },
   });
 
@@ -25,19 +33,21 @@ const UserDetail = () => {
       <div className="flex pl-[10%] pt-[50px] pb-[20px]">
         <img
           className="rounded-full size-[160px] mr-[24px]"
-          src={user.profile}
+          src={user?.profile}
           alt="아이콘"
         />
         <div>
-          <div className="text-[36px] font-bold">{user.nickname}</div>
-          <div className="text-[14px] text-sub-title">{user.email}</div>
-          <div className="pt-[5px] text-[15px] text-sub-title">동영상 8개</div>
+          <div className="text-[36px] font-bold">{user?.nickname}</div>
+          <div className="text-[14px] text-sub-title">{user?.email}</div>
+          <div className="pt-[5px] text-[15px] text-sub-title">
+            동영상 {userPostList?.length}개
+          </div>
         </div>
       </div>
       <hr />
       <div className="grid grid-cols-[repeat(auto-fill,_minmax(300px,_auto))] gap-[20px] mx-[10%] mt-[50px]">
-        {postList?.map((post: IPost) => (
-          <Post post={post} />
+        {userPostList?.map((post: IPost) => (
+          <Post post={post} isUserPost={true} />
         ))}
       </div>
     </div>
