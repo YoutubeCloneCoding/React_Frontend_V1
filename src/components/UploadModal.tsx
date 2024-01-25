ec6f509c06d9e288b441e92c340b6535b51ee72c
youngeunkim <pkhj286@gmail.com>
2024-01-25 10:33:47 +0900

    로그인 안 했을 때 막기

--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -1,29 +1,38 @@
 import back from "assets/delete.svg";
 import Upload from "pages/upload";
+import useUser from "hooks/useUser";
 
 interface ModalProps {
   onClose: () => void;
 }
 
 const Modal = ({ onClose }: ModalProps) => {
+  const { isLogin } = useUser();
+
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-      <div
-        id="scroll"
-        className="flex flex-col w-3/5 h-5/6 rounded-lg bg-white shadow-md "
-      >
-        <div className="flex  border-b-2 border-hr-gray">
-          <p className="flex text-base p-3 text-xl">동영상 업로드</p>
-          <img
-            className="flex cursor-pointer ml-auto p-4"
-            src={back}
-            alt=""
-            onClick={onClose}
-          />
+    <>
+      {isLogin ? (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+          <div
+            id="scroll"
+            className="flex flex-col w-3/5 h-5/6 rounded-lg bg-white shadow-md "
+          >
+            <div className="flex  border-b-2 border-hr-gray">
+              <p className="flex text-base p-3 text-xl">동영상 업로드</p>
+              <img
+                className="flex cursor-pointer ml-auto p-4"
+                src={back}
+                alt=""
+                onClick={onClose}
+              />
+            </div>
+            <Upload />
+          </div>
         </div>
-        <Upload />
-      </div>
-    </div>
+      ) : (
+        alert("로그인이 필요한 작업입니다.")
+      )}
+    </>
   );
 };
 
--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -3,6 +3,7 @@ import upload from "assets/upload.png";
 import customAxios from "lib/customAxios";
 import Detail from "./detail";
 import { useMutation } from "react-query";
+import useUser from "hooks/useUser";
 
 interface VideoDetails {
   id: string;
@@ -12,6 +13,8 @@ interface VideoDetails {
 }
 
 const Upload = () => {
+  const { isLogin } = useUser();
+
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [videoDetails, setVideoDetails] = useState<VideoDetails | null>(null);
   const [contentImageUrl, setContentImageUrl] = useState<string | null>(null);
@@ -56,40 +59,43 @@ const Upload = () => {
   };
 
   return (
-    <div
-      id="scroll"
-      className="flex justify-center items-center flex-col overflow-y-auto max-h-[100vh] overflow-x-hidden p-10"
-      onDrop={(e) => {
-        e.preventDefault();
-        const droppedFile = e.dataTransfer.files[0];
-        handleImageChange(droppedFile);
-        uploadMutation.mutate(droppedFile);
-      }}
-    >
-      {!contentImageUrl && (
-        <div className="flex justify-center items-center flex-col p-10">
-          <img src={upload} alt="업로드" className="w-48" />
-          <p className="mt-2 text-base">
-            동영상 파일을 드래그 앤 드롭하거나 파일을 선택하세요
-          </p>
-          <button className="w-24 h-9 text-base bg-blue-600 text-white border-none rounded-sm mt-5">
-            <label htmlFor="input-file" role="button">
-              파일 선택
-            </label>
-            <input
-              type="file"
-              accept=".mp4, .mkv, .mov"
-              id="input-file"
-              style={{ display: "none" }}
-              aria-hidden
-              ref={fileInputRef}
-              onChange={onContentImageChange}
-            />
-          </button>
-        </div>
-      )}
-      {contentImageUrl && <Detail videoDetails={videoDetails} />}
-    </div>
+    <>
+      {" "}
+      <div
+        id="scroll"
+        className="flex justify-center items-center flex-col overflow-y-auto max-h-[100vh] overflow-x-hidden p-10"
+        onDrop={(e) => {
+          e.preventDefault();
+          const droppedFile = e.dataTransfer.files[0];
+          handleImageChange(droppedFile);
+          uploadMutation.mutate(droppedFile);
+        }}
+      >
+        {!contentImageUrl && (
+          <div className="flex justify-center items-center flex-col p-10">
+            <img src={upload} alt="업로드" className="w-48" />
+            <p className="mt-2 text-base">
+              동영상 파일을 드래그 앤 드롭하거나 파일을 선택하세요
+            </p>
+            <button className="w-24 h-9 text-base bg-blue-600 text-white border-none rounded-sm mt-5">
+              <label htmlFor="input-file" role="button">
+                파일 선택
+              </label>
+              <input
+                type="file"
+                accept=".mp4, .mkv, .mov"
+                id="input-file"
+                style={{ display: "none" }}
+                aria-hidden
+                ref={fileInputRef}
+                onChange={onContentImageChange}
+              />
+            </button>
+          </div>
+        )}
+        {contentImageUrl && <Detail videoDetails={videoDetails} />}
+      </div>
+    </>
   );
 };
 
